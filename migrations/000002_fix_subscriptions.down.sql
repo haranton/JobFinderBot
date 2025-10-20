89a3f46925b2873c8db5da89aec1a90cfb8f3779
haranton2003 <haranton2003@gmai.com>
2025-10-20 13:56:08 +0300

    фикс миграций

--- migrations/000002_fix_subscriptions.down.sql
+++ migrations/000002_fix_subscriptions.down.sql
@@ -2,4 +2,4 @@ ALTER TABLE subscriptions
 DROP CONSTRAINT IF EXISTS unique_user_search;
 
 ALTER TABLE subscriptions
-ADD CONSTRAINT unique_subscription_per_user UNIQUE (telegram_id);
+ADD CONSTRAINT subscriptions_telegram_id_key UNIQUE (telegram_id);
